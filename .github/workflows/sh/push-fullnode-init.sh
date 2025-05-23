c0fe4e404496d4de8c9266001191e457a554b996
alnoki <43892045+alnoki@users.noreply.github.com>
2025-05-23 15:42:30 -0700

    Change script piping

--- .github/workflows/sh/push-fullnode-init.sh
+++ .github/workflows/sh/push-fullnode-init.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 docker tag "$IMAGE_ID" \
-    "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO"
+
+echo "$IMAGE_ID"
+TAG="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO"
+echo $"TAG"
+
 docker images
