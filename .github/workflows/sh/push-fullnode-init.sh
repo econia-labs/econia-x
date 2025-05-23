5d2fe3a69526ac80db0421ef8c9a21c4a30561c6
alnoki <43892045+alnoki@users.noreply.github.com>
2025-05-23 15:45:03 -0700

    Use the right script

--- .github/workflows/sh/push-fullnode-init.sh
+++ .github/workflows/sh/push-fullnode-init.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-docker tag "$IMAGE_ID" \
-
-echo "$IMAGE_ID"
-TAG="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO"
-echo $"TAG"
+docker tag "$IMAGE_ID"  \
+ "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO/foo"
 
 docker images
