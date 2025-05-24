ba044e57bba3615ad9f451c9d50f4847d5df468f
alnoki <43892045+alnoki@users.noreply.github.com>
2025-05-23 17:04:22 -0700

    Try authenticating to AWS

--- .github/workflows/sh/push-fullnode-init.sh
+++ .github/workflows/sh/push-fullnode-init.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-
-echo $TAGS
-
-echo foo
-
 for tag in $TAGS; do
     TAG="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPO:$tag"
     docker tag "$IMAGE_ID" "$TAG"
+    docker push "$TAG"
 done
-
-docker images
