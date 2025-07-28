1d4b1a00af060bc561805245a06e1dac162d98f8
alnoki <43892045+alnoki@users.noreply.github.com>
2025-07-28 14:41:35 -0700

    Begin launch program layout

--- research/prototype/src/market.rs
+++ research/prototype/src/market.rs
@@ -2,37 +2,45 @@ use solana_program::{
     account_info::AccountInfo, entrypoint::ProgramResult, program_error::ProgramError,
     pubkey::Pubkey,
 };
+use std::mem::size_of;
+
 struct Market {
     base_mint: Pubkey,
     quote_mint: Pubkey,
 }
-use std::mem::size_of;
 
 impl Market {
     /// Derive the market address from the base and quote mint addresses.
-    pub fn address(&self, program_id: &Pubkey) -> Pubkey {
+    fn address(&self, program_id: &Pubkey) -> Pubkey {
         let (address, _bump_seed) = Pubkey::find_program_address(
             &[&self.base_mint.to_bytes(), &self.quote_mint.to_bytes()],
             program_id,
         );
         address
     }
+
+    fn from(instruction: LaunchInstruction) -> Self {
+        Self {
+            base_mint: instruction.base_mint,
+            quote_mint: instruction.quote_mint,
+        }
+    }
 }
 
-struct LaunchParameters {
+struct LaunchInstruction {
     base_mint: Pubkey,
     quote_mint: Pubkey,
 }
 
-impl LaunchParameters {
+impl LaunchInstruction {
     fn unpack(bytes: &[u8]) -> Result<Self, ProgramError> {
         if bytes.len() != std::mem::size_of::<Self>() {
             return Err(ProgramError::InvalidInstructionData);
         }
         let base_bytes_ptr = bytes.as_ptr() as *const [u8; size_of::<Pubkey>()];
         unsafe {
-            // Since the number of bytes in the instruction data has already been verified, raw
-            // pointer dereferencing is safe here.
+            // Since the number of bytes in the instruction data bytes has already been verified,
+            // raw pointer arithmetic and dereferencing is safe here.
             let quote_bytes_ptr = base_bytes_ptr.add(size_of::<Pubkey>());
             Ok({
                 Self {
@@ -45,10 +53,10 @@ impl LaunchParameters {
 }
 
 pub(crate) fn launch(
-    _program_id: &Pubkey,
+    program_id: &Pubkey,
     _accounts: &[AccountInfo],
-    launch_parameter_bytes: &[u8],
+    instruction_parameter_bytes: &[u8],
 ) -> ProgramResult {
-    let launch_parameters = LaunchParameters::unpack(launch_parameter_bytes)?;
+    let market = Market::from(LaunchInstruction::unpack(instruction_parameter_bytes)?);
     Ok(())
 }
