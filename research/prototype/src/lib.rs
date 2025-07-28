597489523d4fc122bb32d106a6490d3094457633
alnoki <43892045+alnoki@users.noreply.github.com>
2025-07-28 13:24:03 -0700

    Abstract try_from

--- research/prototype/src/lib.rs
+++ research/prototype/src/lib.rs
@@ -13,19 +13,28 @@ enum InstructionType {
     LaunchMarket = 0,
 }
 
+impl TryFrom<&u8> for InstructionType {
+    type Error = ProgramError;
+
+    fn try_from(value: &u8) -> Result<Self, Self::Error> {
+        match value {
+            val if *val == InstructionType::LaunchMarket as u8 => Ok(InstructionType::LaunchMarket),
+            _ => Err(ProgramError::InvalidInstructionData),
+        }
+    }
+}
+
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let (&instruction_type_byte, instruction_parameter_bytes) = instruction_data
+    let (&instruction_type_byte, parameter_bytes) = instruction_data
         .split_first()
         .ok_or(ProgramError::InvalidInstructionData)?;
-    match instruction_type_byte {
-        val if val == InstructionType::LaunchMarket as u8 => {
-            market::launch(program_id, accounts, instruction_parameter_bytes)?;
-        }
-        _ => return Err(ProgramError::InvalidInstructionData),
+    let instruction_type = InstructionType::try_from(&instruction_type_byte)?;
+    match instruction_type {
+        InstructionType::LaunchMarket => market::launch(program_id, accounts, parameter_bytes)?,
     }
     Ok(())
 }
